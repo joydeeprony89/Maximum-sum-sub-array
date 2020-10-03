d034f9521ac10d6fc1c5d446bd77d03bb3dd9f13
Jaydeep Shil <joydeeprony89@gmail.com>
2020-10-03 12:24:44 +0530

    Add project files.

---
+++ Maximum sum sub-array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Maximum_sum_sub_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            Console.WriteLine(MaxSubArray(nums));
+        }
+
+        static int MaxSubArray(int[] nums)
+        {
+            int currentMax = nums[0];
+            int globalMax = nums[0];
+            for(int i = 1; i< nums.Length; ++i)
+            {
+                currentMax = Math.Max(nums[i], nums[i] + currentMax);
+                globalMax = Math.Max(currentMax, globalMax);
+            }
+
+            return globalMax;
+        }
+    }
+}
